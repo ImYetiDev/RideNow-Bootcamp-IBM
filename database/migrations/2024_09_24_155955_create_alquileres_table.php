51ce3ee0bc634123d090ab99f8981d9b9787664a
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-09-24 11:03:35 -0500

    migraciones

--- database/migrations/2024_09_24_155955_create_alquileres_table.php
+++ database/migrations/2024_09_24_155955_create_alquileres_table.php
@@ -1,27 +1,32 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateAlquileresTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('alquileres', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->unsignedBigInteger('bicicleta_id');
+            $table->unsignedBigInteger('estacion_inicio_id');
+            $table->unsignedBigInteger('estacion_fin_id')->nullable();
+            $table->timestamp('fecha_inicio')->useCurrent();
+            $table->timestamp('fecha_fin')->nullable();
+            $table->enum('estado', ['en curso', 'completado', 'cancelado'])->default('en curso');
             $table->timestamps();
+
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->foreign('bicicleta_id')->references('id')->on('bicicletas')->onDelete('cascade');
+            $table->foreign('estacion_inicio_id')->references('id')->on('estaciones')->onDelete('cascade');
+            $table->foreign('estacion_fin_id')->references('id')->on('estaciones')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('alquileres');
     }
-};
+}
--- database/migrations/2024_09_24_160003_create_mantenimiento_table.php
+++ database/migrations/2024_09_24_160003_create_mantenimiento_table.php
@@ -1,27 +1,26 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateMantenimientoTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('mantenimiento', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('bicicleta_id');
+            $table->text('descripcion')->nullable();
+            $table->timestamp('fecha')->useCurrent();
             $table->timestamps();
+
+            $table->foreign('bicicleta_id')->references('id')->on('bicicletas')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('mantenimiento');
     }
-};
+}
+
